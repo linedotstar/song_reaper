a0537950572b90d4df52ce9dcf6b5ba35313f697
Chris Friedrich <linedotstar@gmail.com>
2012-06-02 14:59:55 -0700

    isolate client sockets by socket.io namespace

--- app.js
+++ app.js
@@ -1,22 +1,18 @@
-var app = require('express').createServer(),
+var express = require('express'),
+    app = express.createServer(),
+    io = require('socket.io').listen(app),
     config = require('config'),
-    fs = require("fs"),
-    serviceHub = require('./lib/services/serviceHub').serviceHub,
-    services;
-
-services = [
-  'rdio',
-  // 'soundcloud',
-  'spotify'
-];
-for(var i = 0; i < services.length; i++) {
-  serviceHub.registerService(services[i]);
-}
+    ServiceHub = require('./lib/services/serviceHub').ServiceHub;
 
 app.set('view engine', 'jade');
+app.use(express.cookieParser());
+app.use(express.session({secret: config.server.secret}))
 
 app.get('/', function (req, res) {
-  res.render('index');
+  var channel = Math.random(),
+      hub = new ServiceHub();
+  initSocket(hub, channel);
+  res.render('index', {channel: 'http://' + config.server.host + '/' + channel});
 });
 
 app.listen(config.server.port);
@@ -24,15 +20,17 @@ console.log('app started');
 
 // Socket.io
 // -------------------------------------------------- //
-
-var io = require('socket.io').listen(app);
-
-io.sockets.on('connection', function(socket) {
-  socket.on('search', function(data) {
-    console.log(data);
-    serviceHub.search(data.types, data.query);
+var initSocket = function(serviceHub, channel) {
+  console.log('Initializing socket on channel ' + channel);
+  io.of('/' + channel).on('connection', function(socket) {
+    socket.on('search', function(data) {
+      console.log('Accepting: ' + data + ' on channel ' + channel);
+      serviceHub.search(data.types, data.query);
+    });
+    serviceHub.on('results', function(data) {
+      console.log('Emitting: ' + data + ' on channel ' + channel);
+      socket.emit('results', data);
+    });
   });
-  serviceHub.on('results', function(data) {
-    socket.emit('results', data);
-  });
-});
+  return channel;
+}
